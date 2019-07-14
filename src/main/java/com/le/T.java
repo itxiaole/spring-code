76a92fff719481ccb5d111a00bf4fffa9f9b9332
Administrator <xiaolev5@163.com>
2019-07-14 23:13:08 +0800

    aaa

---
+++ src/main/java/com/le/T.java
@@ -0,0 +1,9 @@
+package com.le;
+
+public class T {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("1234");
+        System.out.println("12345");
+    }
+}
